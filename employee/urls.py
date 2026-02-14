8ad30d9d72787c2316d7d293fe07403490e031ad
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2026-02-14 21:08:20 +0530

    added PUT, GET, POST, DELETE methods

--- employee/urls.py
+++ employee/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('employees/', views.EmployeeListCreate.as_view(), name='employee-list-create'),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view(), name='employee-detail'),
 
 ]
